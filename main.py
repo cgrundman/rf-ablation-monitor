70cf3df13234e6305a5801f49992d942b58d8470
cgrundman <92883095+cgrundman@users.noreply.github.com>
2025-01-16 22:12:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import tkinter as tk
 from gui.application import Application
 
 if __name__ == "__main__":
-    app = Application()
-    app.run()
+    root = tk.Tk()
+    app = Application(root)
+    root.mainloop()
